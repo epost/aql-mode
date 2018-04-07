2f63dc85cabb54debe35602be10c4c296248e1c2
Erik Post <erik@shinsetsu.nl>
2018-04-07 17:42:49 +0200

    Add syntax.

--- aql-mode.el
+++ aql-mode.el
@@ -4,6 +4,7 @@
 ;; References:
 ;;  - http://www.wilfred.me.uk/blog/2015/03/19/adding-a-new-language-to-emacs/
 ;;  - https://www.emacswiki.org/emacs/EmacsSyntaxTable
+;;  - https://github.com/CategoricalData/fql/blob/2df5b37f765e850ccb9a4f9e0c361179d6f484fd/src/catdata/aql/exp/AqlParser.java
 
 (defconst aql-mode-syntax-table
   (let ((table (make-syntax-table java-mode-syntax-table)))
@@ -15,8 +16,14 @@
 
 (setq aql-highlights
       '(
-        ("pragma\\|typeside\\|schema\\|instance\\|literal\\|exec_jdbc\\|import_jdbc\\|export_jdbc_instance\\|entities\\|foreign_keys\\|attributes\\|java_types\\|java_constants" . font-lock-keyword-face)
-        (":\\|->" . font-lock-builtin-face)
+        ("identity\\|literal\\|sql\\|schemaOf\\|typesideOf\\|toQuery\\|fromQuery\\|simple\\|chase\\|anonymize\\|frozen\\|unit\\|counit_query\\|counit\\|empty\\|union\\|coproduct\\|coproduct_sigma\\|distinct\\|eval\\|coeval\\|coequalize\\|src\\|dst\\|check\\|assert_consistent" . font-lock-constant-face)
+        ("pragma\\|command\\|typeside\\|schema\\|instance\\|mapping\\|transform\\|constraints\\|query\\|graph\\|exec_jdbc\\|exec_js\\|exec_cmdline\\|import_csv\\|import_jdbc\\|export_jdbc_instance" . font-lock-keyword-face)
+        ("html\\|md" . font-lock-keyword-face)
+        ("from\\|where\\|exists" . font-lock-type-face)
+        ("nodes\\|edges\\|entity\\|entities\\|foreign_keys\\|attributes\\|generators\\|equations\\|multi_equations\\|path_equations\\|observation_equations\\|options\\|functions\\|constants\\|types\\|imports\\|java_types\\|java_constants\\|java_functions" . font-lock-function-name-face)
+        ("forall" . font-lock-variable-name-face)
+        ("delta\\|lambda\\|pi\\|sigma" . font-lock-builtin-face)
+        (":\\|->\\|;" . font-lock-builtin-face)
         ))
 
 (define-derived-mode aql-mode fundamental-mode "aql"
